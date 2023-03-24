3adde4356e8f32f303ce2c09c52c624b1d525fcf
Uma <umashree31jan@gmail.com>
2023-03-24 17:27:30 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,18 +1,4 @@
-<?php require_once('functions/db.php');
-require_once('db.php');
-
-
-$Uname=$_POST['username'];
-$Password=$_POST['password'];
-
-$username=stripcslashes($Uname);
-$password=stripcslashes($Password);
-$username=mysqli_real_escape_string($con,$username);
-$Password=mysqli_real_escape_string($con,$password);
-
-
-$sql="select* from `claimant-details` where User_Name=$username";
-$result=mysql_select_sdb()
+<?php 
 
 
 ?>
